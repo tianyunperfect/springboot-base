a533325879dc6a26a6d5aab0563ae3a289843e0e
tianyunperfect <tianyunperfect@gmail.com>
2020-11-06 18:15:49 +0800

    添加baseController

---
+++ springboot-main/src/main/java/com/alvin/controller/BaseController.java
@@ -0,0 +1,24 @@
+package com.alvin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Controller
+@CrossOrigin
+public class BaseController {
+    protected HttpServletRequest request;
+    protected HttpServletResponse response;
+    protected HttpSession session;
+
+    @ModelAttribute
+    public void setReqAndRes(HttpServletRequest request, HttpServletResponse response){
+        this.request = request;
+        this.response = response;
+        this.session = request.getSession();
+    }
+}
